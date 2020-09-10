df1d508de69a0e7d46c99088c3aa557f02ac787c
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-10 17:35:04 +0100

    Update function to sort array based on property

--- spec/sortByObjectProperty.spec.js
+++ spec/sortByObjectProperty.spec.js
@@ -3,6 +3,12 @@ var sortByObjectProperty = require('../src/sortByObjectProperty');
 describe('sortByObjectProperty', () => {
     it('returns the same for an array of one object', () => {
         var array = [{name:'Liz'}]
-        expect(sortByObjectProperty(array)).toEqual(array);
+        expect(sortByObjectProperty(array, 'name')).toEqual(array);
+    })
+
+    it('sorts an array of two objects with one property', () => {
+        var input = [{name:'Liz'},{name:'Becky'}]
+        var expected = [{name:'Becky'},{name:'Liz'}]
+        expect(sortByObjectProperty(input, 'name')).toEqual(expected)
     })
 })
--- src/sortByObjectProperty.js
+++ src/sortByObjectProperty.js
@@ -1,5 +1,24 @@
-function sortByObjectProperty(array) {
+function sortByObjectProperty(array, property) {
+    // built in method of sorting
+    // return array.sort((a,b) => {
+    //     return a[property] > b[property] ? 1 : -1;
+    // })
+
+    // algorithm to sort using insertion method (in-built method uses this)
+    for(var i = 0; i < array.length; i++) {
+        var item = array[i];
+        for(var place = i + 1; place < array.length; place++) {
+            var nextItem = array[place];
+            if(item[property] > array[place][property]) {
+                array[i] = nextItem;
+                array[place] = item;
+                item = array[i];
+            }
+        }
+    }
     return array;
 }
+
+
 
 module.exports = sortByObjectProperty;
