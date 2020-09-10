e3ad3308322c16be4baa716e7620c8d55e98f14c
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-10 15:44:33 +0100

    Fix indentation

--- spec/getObjectLength.spec.js
+++ spec/getObjectLength.spec.js
@@ -1,19 +1,18 @@
 var getObjectLength = require('../src/getObjectLength')
 
 describe('getObjectLength', () => {
-    it('returns 0 for an empty object', () => {
-        var object = {};
-        expect(getObjectLength(object)).toEqual(0);
-    })
+  it('returns 0 for an empty object', () => {
+    var object = {};
+    expect(getObjectLength(object)).toEqual(0);
+  })
 
-    it('returns 1 for an object with one property', ()=> {
-        var object = { name: 'Liz'};
-        expect(getObjectLength(object)).toEqual(1);
-    })
+  it('returns 1 for an object with one property', ()=> {
+    var object = { name: 'Liz'};
+    expect(getObjectLength(object)).toEqual(1);
+  })
 
-    it('counts multiple properties in an object', () => {
-        var object = {name: 'test', class: 'VI', rollno: 12 };
-        expect(getObjectLength(object)).toEqual(3);
-    })
-
+  it('counts multiple properties in an object', () => {
+    var object = {name: 'test', class: 'VI', rollno: 12 };
+    expect(getObjectLength(object)).toEqual(3);
+  })
 })
