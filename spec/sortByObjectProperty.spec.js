8d2a4d0bc556d49159550e4cef6bd58e1222f50c
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-10 17:44:11 +0100

    Fix linting errors

--- spec/sortByObjectProperty.spec.js
+++ spec/sortByObjectProperty.spec.js
@@ -1,14 +1,31 @@
 var sortByObjectProperty = require('../src/sortByObjectProperty');
 
 describe('sortByObjectProperty', () => {
-    it('returns the same for an array of one object', () => {
-        var array = [{name:'Liz'}]
-        expect(sortByObjectProperty(array, 'name')).toEqual(array);
-    })
+  it('returns the same for an array of one object', () => {
+    var input = [{ name: 'Liz' }];
+    expect(sortByObjectProperty(array, 'name')).toEqual(array);
+  });
 
-    it('sorts an array of two objects with one property', () => {
-        var input = [{name:'Liz'},{name:'Becky'}]
-        var expected = [{name:'Becky'},{name:'Liz'}]
-        expect(sortByObjectProperty(input, 'name')).toEqual(expected)
-    })
-})
+  it('sorts an array of two objects with one property', () => {
+    var input = [{ name: 'Liz' }, { name: 'Becky' }];
+    var expected = [{ name: 'Becky' }, { name: 'Liz' }];
+    expect(sortByObjectProperty(input, 'name')).toEqual(expected);
+  });
+
+  it('sorts an array of object swith more than one property', () => {
+    var input = [
+      { id: 143, name: 'Becky' },
+      { id: 123, name: 'Liz' },
+    ];
+    var expected = [
+      { id: 123, name: 'Liz' },
+      { id: 143, name: 'Becky' },
+    ];
+    expect(sortByObjectProperty(input, 'id')).toEqual(expected);
+  });
+
+  it('shows error message if property does not exist', () => {
+
+  })
+});
+
--- src/sortByObjectProperty.js
+++ src/sortByObjectProperty.js
@@ -1,24 +1,22 @@
 function sortByObjectProperty(array, property) {
-    // built in method of sorting
-    // return array.sort((a,b) => {
-    //     return a[property] > b[property] ? 1 : -1;
-    // })
+  // built in method of sorting
+  // return array.sort((a,b) => {
+  //     return a[property] > b[property] ? 1 : -1;
+  // })
 
-    // algorithm to sort using insertion method (in-built method uses this)
-    for(var i = 0; i < array.length; i++) {
-        var item = array[i];
-        for(var place = i + 1; place < array.length; place++) {
-            var nextItem = array[place];
-            if(item[property] > array[place][property]) {
-                array[i] = nextItem;
-                array[place] = item;
-                item = array[i];
-            }
-        }
+  // algorithm to sort using insertion method (in-built method uses this)
+  for (var i = 0; i < array.length; i++) {
+    var item = array[i];
+    for (var place = i + 1; place < array.length; place++) {
+      var nextItem = array[place];
+      if (item[property] > array[place][property]) {
+        array[i] = nextItem;
+        array[place] = item;
+        item = array[i];
+      }
     }
-    return array;
+  }
+  return array;
 }
-
-
 
 module.exports = sortByObjectProperty;
