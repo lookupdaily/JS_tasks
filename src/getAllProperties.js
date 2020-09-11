e5fb771a42cc6449021102484267a0332fe5a9d1
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-11 17:40:49 +0100

    Revert changes to master

--- src/getAllProperties.js
+++
@@ -1,9 +0,0 @@
-function getAllProperties(object) {
-  var properties = [];
-  for (var property in object) {
-    properties.push(property);
-  }
-  return properties;
-}
-
-module.exports = getAllProperties;
--- src/getObjectLength.js
+++
@@ -1,9 +0,0 @@
-function getObjectLength(object) {
-  var noOfProperties = 0;
-  for (var property in object) {
-    noOfProperties += 1;
-  }
-  return noOfProperties;
-}
-
-module.exports = getObjectLength;
