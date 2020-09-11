5d62cc732a9729d291c73bdabca4efa1ad59bdee
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-11 09:47:23 +0100

    Test for edge cases

--- spec/getAllProperties.spec.js
+++ spec/getAllProperties.spec.js
@@ -7,10 +7,30 @@ describe('getAllProperties', () => {
   });
 
   it('returns all properties of an object with inherited properties', () => {
-    var Parent = { library: 'Stockport', inStock: true};
+    var Parent = { library: 'Stockport', inStock: true };
     var child = Object.create(Parent);
     child.title = 'The Road Ahead';
     // sort arrays to check for an exact match of properties
-    expect(getAllProperties(child).sort()).toEqual(['library','inStock','title'].sort());
+    expect(getAllProperties(child).sort()).toEqual(
+      ['library', 'inStock', 'title'].sort()
+    );
+  });
+
+  it('works for multiple parents', () => {
+    var Parent = { library: 'Stockport', inStock: true };
+    var child = Object.create(Parent);
+    child.title = 'The Road Ahead';
+    var childOfChild = Object.create(child);
+    childOfChild.id = 1254;
+
+    expect(getAllProperties(childOfChild).sort()).toEqual(
+      ['library', 'inStock', 'title', 'id'].sort()
+    );
+  });
+
+  it('can handle empty objects', () => {
+    var Parent = {};
+    var child = Object.create(Parent);
+    expect(getAllProperties(child)).toEqual([]);
   })
 });
