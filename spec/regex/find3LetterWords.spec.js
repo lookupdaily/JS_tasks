3a187301f45aa19edfc1e61ac5f14ca0537e1047
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-15 11:08:04 +0100

    Update function to return only 3 letter words

--- spec/regex/find3LetterWords.spec.js
+++ spec/regex/find3LetterWords.spec.js
@@ -4,4 +4,8 @@ describe('find three letter word', () => {
   it('returns three letter word in three letter string', () => {
     expect(find3LetterWords('the')).toEqual('the');
   });
+
+  it('returns only the three letter word in a two word string', () => {
+    expect(find3LetterWords('the quick')).toEqual('the');
+  })
 });
--- src/regex/find3LetterWords.js
+++ src/regex/find3LetterWords.js
@@ -1,5 +1,14 @@
 function find3LetterWords(string) {
-  return string;
+  let words = string.split(' ');
+  let output = [];
+
+  words.forEach(word => {
+    if(word.length === 3) {
+      output.push(word);
+    }
+  })
+
+  return output.join(' ');
 }
 
 module.exports = find3LetterWords;
