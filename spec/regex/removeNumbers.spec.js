4dbc4ce45d34cb88df0dd9f42e8123cc13cfcbdd
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-15 17:55:11 +0100

    Test for capital letters

--- spec/regex/removeNumbers.spec.js
+++ spec/regex/removeNumbers.spec.js
@@ -7,9 +7,13 @@ describe('remove numbers', () => {
 
   it('removes one number from a string with a single letter and digit', () => {
     expect(removeNumbers('a1')).toEqual('a');
-  })
+  });
 
   it('removes more than one number from a string with multiple numbers', () => {
-    expect(removeNumbers('a12b')).toEqual('ab')
-  })
+    expect(removeNumbers('a12b')).toEqual('ab');
+  });
+
+  it('works with capital letters', () => {
+    expect(removeNumbers('A1')).toEqual('A');
+  });
 });
