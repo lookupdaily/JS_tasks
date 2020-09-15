494014dbb9ab9a9d02ac08a5405bd499505084e7
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-15 18:46:15 +0100

    Return only monetary value from string

--- spec/regex/getMonetaryValue.spec.js
+++ spec/regex/getMonetaryValue.spec.js
@@ -2,6 +2,10 @@ const getMonetaryValue = require('../../src/regex/getMonetaryValue');
 
 describe('get monetary value', () => {
   it('returns monetary value when only value in string', () => {
-    expect(getMonetaryValue('$9.99')).toEqual('$9.99');
+    expect(getMonetaryValue('$9.99')).toEqual(['$9.99']);
+  });
+
+  it('removes non monetary value from string', () => {
+    expect(getMonetaryValue('price $9.99')).toEqual(['$9.99']);
   });
 });
--- src/regex/getMonetaryValue.js
+++ src/regex/getMonetaryValue.js
@@ -1,5 +1,6 @@
 function getMonetaryValue(string) {
-  return string;
+  const regex = /\$+([0-9]{1,})+(\.+[0-9]{0,2})?/
+  return [string.match(regex)[0]];
 }
 
 module.exports = getMonetaryValue;
