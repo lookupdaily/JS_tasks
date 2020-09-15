a6d337c2392e09c370263a1730e3bb5612c081f6
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-15 18:56:52 +0100

    Update function to return array of monetary values

--- src/regex/getMonetaryValue.js
+++ src/regex/getMonetaryValue.js
@@ -1,6 +1,6 @@
 function getMonetaryValue(string) {
-  const regex = /\$+([0-9]{1,})+(\.+[0-9]{0,2})?/
-  return [string.match(regex)[0]];
+  const regex = /\$+(?:[0-9]{1,})+(?:\.+[0-9]{0,2})?/g
+  return string.match(regex);
 }
 
 module.exports = getMonetaryValue;
