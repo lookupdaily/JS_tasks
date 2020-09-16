3247fa0bbd6b4c8d8913756ee7badafe7b1880a3
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-16 09:09:40 +0100

    Update 3Letter word function to return regex output

--- spec/regex/find3LetterWords.spec.js
+++ spec/regex/find3LetterWords.spec.js
@@ -2,19 +2,19 @@ const find3LetterWords = require('../../src/regex/find3LetterWords');
 
 describe('find three letter word', () => {
   it('returns three letter word in three letter string', () => {
-    expect(find3LetterWords('the')).toEqual('the');
+    expect(find3LetterWords('the')).toEqual(['the']);
   });
 
   it('returns only the three letter word in a two word string', () => {
-    expect(find3LetterWords('the quick')).toEqual('the');
+    expect(find3LetterWords('the quick')).toEqual(['the']);
   });
 
   it('returns more than one three letter word', () => {
-    expect(find3LetterWords('the quick brown fox')).toEqual('the fox');
+    expect(find3LetterWords('the quick brown fox')).toEqual(['the', 'fox']);
   });
 
   it('ignores capital letters', () => {
-    expect(find3LetterWords('The')).toEqual('The');
+    expect(find3LetterWords('The')).toEqual(['The']);
   });
 
   it('displays a message if no matches', () => {
@@ -25,6 +25,6 @@ describe('find three letter word', () => {
 
   it('passes supplied test case', () => {
     const string = 'The quick brown fox jumped over the lazy dog';
-    expect(find3LetterWords(string)).toEqual('The fox the dog');
+    expect(find3LetterWords(string)).toEqual(['The', 'fox', 'the', 'dog']);
   });
 });
--- src/regex/find3LetterWords.js
+++ src/regex/find3LetterWords.js
@@ -2,7 +2,7 @@ function find3LetterWords(string) {
   const regex = /\b[A-Za-z]{3}\b/g;
   const matchingWords = string.match(regex);
   if (matchingWords) {
-    return matchingWords.join(' ');
+    return matchingWords;
   } else {
     return 'No three letter word found in string';
   }
