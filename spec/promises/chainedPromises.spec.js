7ecb526700356af993f2ef83bea584eb7b8a4342
Liz Daly <liz7daly@gmail.com>
2020-09-17 22:54:24 +0100

    Create sort function

--- spec/promises/chainedPromises.spec.js
+++ spec/promises/chainedPromises.spec.js
@@ -1,5 +1,6 @@
 const chainedPromises = require('../../src/promises/chainedPromises');
 const capitalize = chainedPromises.capitalize;
+const sort = chainedPromises.sort;
 
 describe('capitalize', () => {
   it('capitalizes array of one', async () => {
@@ -12,5 +13,15 @@ describe('capitalize', () => {
     await capitalize(['cat', 'dog']).then((result) => {
       expect(result).toEqual(['Cat', 'Dog']);
     });
+  });
+
+  it('can be chained with sort function', async () => {
+    await capitalize(['dog', 'cat', 'Fox'])
+      .then((result) => {
+        return sort(result);
+      })
+      .then((result) => {
+        expect(result).toEqual(['Cat', 'Dog', 'Fox']);
+      });
   });
 });
--- src/promises/chainedPromises.js
+++ src/promises/chainedPromises.js
@@ -1,11 +1,17 @@
 const capitalize = (words) => {
-  const capitalizedWords = words.map(word => {
-    return word[0].toUpperCase() + word.substring(1)
-  })
+  const capitalizedWords = words.map((word) => {
+    return word[0].toUpperCase() + word.substring(1);
+  });
 
   return new Promise((resolve) => {
     resolve(capitalizedWords);
   });
 };
 
-module.exports = { capitalize };
+const sort = (words) => {
+  return new Promise((resolve) => {
+    resolve(words.sort());
+  });
+};
+
+module.exports = { capitalize, sort };
