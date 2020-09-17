23945c21931b9ccbdf818ba11766620f6e388b00
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-17 16:45:54 +0100

    Update function to accept special characters

--- spec/regex/getEmail.spec.js
+++ spec/regex/getEmail.spec.js
@@ -20,4 +20,34 @@ describe('getEmail', () => {
       getEmail('james@juniordevelopercentral.com or james@yahoo.com')
     ).toEqual(['james@juniordevelopercentral.com', 'james@yahoo.com']);
   });
+
+  it('accepts "-" in first section of email', () => {
+    expect(getEmail('james-marshall@developer.com')).toEqual([
+      'james-marshall@developer.com',
+    ]);
+  });
+
+  it('accepts "_" in first section of email', () => {
+    expect(getEmail('james_marshall@developer.com')).toEqual([
+      'james_marshall@developer.com',
+    ]);
+  });
+
+  it('accepts "." in first section of email', () => {
+    expect(getEmail('james.marshall@developer.com')).toEqual([
+      'james.marshall@developer.com',
+    ]);
+  });
+
+  it('accepts "-" in second section of email', () => {
+    expect(getEmail('jamesmarshall@developer-central.com')).toEqual([
+      'jamesmarshall@developer-central.com',
+    ]);
+  });
+
+  it('accepts "." in section section of email', () => {
+    expect(getEmail('jamesmarshall@developer.co.uk')).toEqual([
+      'jamesmarshall@developer.co.uk',
+    ]);
+  });
 });
--- src/regex/getEmail.js
+++ src/regex/getEmail.js
@@ -1,5 +1,5 @@
 function getEmail(string) {
-  const regex = /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}/g;
+  const regex = /[a-zA-Z0-9-_.]+@[a-zA-Z0-9-.]+\.[a-zA-Z]{2,4}/g;
   return string.match(regex);
 }
 
