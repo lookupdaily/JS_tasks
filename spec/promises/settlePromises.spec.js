4be175247d0e1ab8ad83efe233be09933b6f93e8
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-18 09:31:07 +0100

    Create function to test status of promises

--- spec/promises/settlePromises.spec.js
+++ spec/promises/settlePromises.spec.js
@@ -1,4 +1,6 @@
-const settleAll = require('../../src/promises/settlePromises');
+const promiseFunctions = require('../../src/promises/settlePromises');
+const settleAll = promiseFunctions.settleAll;
+const checkStatuses = promiseFunctions.checkStatuses;
 
 describe('settleAll', () => {
   it('returns an array of multiple settled promises', async () => {
@@ -11,8 +13,8 @@ describe('settleAll', () => {
     const checkGreaterThan10 = (num) => {
       return new Promise((resolve, reject) => {
         num > 10
-          ? resolve('greater than 10')
-          : reject('must be greater than 10');
+          ? resolve('value is greater than 10')
+          : reject('value must be greater than 10');
       });
     };
 
@@ -20,7 +22,51 @@ describe('settleAll', () => {
       settleAll(delay(3000), checkGreaterThan10(11))
     ).resolves.toEqual([
       { status: 'fulfilled', value: undefined },
-      { status: 'fulfilled', value: 'greater than 10' },
+      { status: 'fulfilled', value: 'value is greater than 10' },
     ]);
+  });
+
+  it('includes rejected promises', async () => {
+    const delay = (ms) => {
+      return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+      });
+    };
+
+    const checkGreaterThan10 = (num) => {
+      return new Promise((resolve, reject) => {
+        num > 10
+          ? resolve('value is greater than 10')
+          : reject('value must be greater than 10');
+      });
+    };
+
+    await expect(
+      settleAll(delay(3000), checkGreaterThan10(1))
+    ).resolves.toEqual([
+      { status: 'fulfilled', value: undefined },
+      { status: 'rejected', reason: 'value must be greater than 10' },
+    ]);
+  });
+
+  it('can be used to check the status of a list of promises', async () => {
+    const delay = (ms) => {
+      return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+      });
+    };
+
+    const checkGreaterThan10 = (num) => {
+      return new Promise((resolve, reject) => {
+        num > 10
+          ? resolve('value is greater than 10')
+          : reject('value must be greater than 10');
+      });
+    };
+
+    await expect(checkStatuses(
+      delay(2000),
+      checkGreaterThan10(1)
+    )).resolves.toEqual(['fulfilled', 'rejected']);
   });
 });
--- src/promises/settlePromises.js
+++ src/promises/settlePromises.js
@@ -2,4 +2,10 @@ const settleAll = async (...args) => {
   return Promise.allSettled(args).then((values) => values);
 };
 
-module.exports = settleAll;
+const checkStatuses = async (...args) => {
+  return Promise.allSettled(args).then((values) => {
+    return values.map(value => value.status);
+  })
+}
+
+module.exports = {settleAll, checkStatuses};
